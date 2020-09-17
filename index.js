ce77dbd32ead428a361f6189971ffceff8a40899
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-09-18 02:46:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -852,31 +852,10 @@ if(message.content == 'o!솔저') {
 
     message.channel.send(embed)
   }
-if(message.content == 'o!이스터에그') {
-    let img = 'https://cdn.discordapp.com/attachments/726255182484209756/749814328735170590/aasdzxc.png';
-    let embed = new Discord.RichEmbed()
-      .setURL('http://www.naver.com')
-      .setColor('#ffff00')
-      .setAuthor('개발자가 숨겨놓은 이스터에그를 찾아보시오', img, )
-      .addField('```이스터에그```', '```찾아보세요```', true)
-      .setThumbnail(img)
 
-    message.channel.send(embed)
-  }
 
   
-  if(message.content == 'o!esp') {
-    let img = 'https://cdn.discordapp.com/attachments/726255182484209756/749814328735170590/aasdzxc.png';
-    let embed = new Discord.RichEmbed()
-      .setURL('http://www.naver.com')
-      .setColor('#ffff00')
-
-      .addField('```ESP```', '```good Esp```', true)
-      .setImage('https://cdn.discordapp.com/attachments/751809877135065120/752076619203477574/esp.png')
-      .setThumbnail(img)
-
-    message.channel.send(embed)
-  }
+  
 
 
 
@@ -889,15 +868,13 @@ if(message.content == 'o!이스터에그') {
         let helpImg = 'https://cdn.discordapp.com/attachments/726255182484209756/749814328735170590/aasdzxc.png';
         let commandList = [
           {name: 'o!(사용할 영웅)', desc: '사용할 영웅 값을 보내줍니다'},
-          {name: 'o!한국어', desc: 'UI 번역을 합니다'},
-          {name: 'o!값', desc: '쉽게 복붙 할수있습니다'},
-          {name: 'o!이스터에그', desc: '개발자에 이스터에그'},
-          {name: 'o!ESP', desc: '예쁜 ESP를 '}, 
         ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
       .setAuthor('ORION BOT HELP USER', helpImg)
       .setColor('#fff000')
+      .addField('https://cdn.discordapp.com/attachments/748535337244033095/748955743461441640/UndetectedSawN.png', '', true)
+
 
       .setTimestamp()
     
