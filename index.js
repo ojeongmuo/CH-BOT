87a09824c857c63d340471e6e94d544b50186af3
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-09-18 17:44:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -863,7 +863,7 @@ if(message.content == 'o!play') {
       .setColor('#FFFF00')
       .setImage('https://cdn.discordapp.com/attachments/755758873335300320/756428964729323550/r3gZGHB.gif')
       .setTitle('Orion Function')
-      .addField('Orion is Overwatch memory aimbot.', ' ')
+      .addField('Orion is Overwatch memory aimbot.', ')
 .addField('Orion is one of the most advanced and customizable.', ' )
 .addField('cheat with the highest level of security.', ' )
 .addField('You will get a powerful performace and easy to use', ' )
