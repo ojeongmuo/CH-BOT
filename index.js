9644b59f4b5a42b76a1267a17f45bdb52b2a2189
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-10-02 21:17:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ if(message.content == 'ch!위도1') {
       .setColor('#ff0000')
       .addField('```Hero Name```', '```fix\nWidow```', true)
       .setThumbnail(img)
-      .setImage('https://cdn.discordapp.com/attachments/757614927489007809/761561217118502933/1.png')
+      .setImage('https://cdn.discordapp.com/attachments/757614927489007809/761562244261347358/dbdb1b557161eee6.png')
       
     message.channel.send(embed)
   }
@@ -202,7 +202,7 @@ if(message.content == 'ch!위도우1') {
       .setColor('#ff0000')
       .addField('```Hero Name```', '```fix\nWidow```', true)
       .setThumbnail(img)
-      .setImage('https://cdn.discordapp.com/attachments/757614927489007809/761561217118502933/1.png')
+      .setImage('https://cdn.discordapp.com/attachments/757614927489007809/761562244261347358/dbdb1b557161eee6.png')
       
     message.channel.send(embed)
   }
